6f7ca9e8e8b8c2633b93aff5e6808711ddf595d1
hanleiwen <wenhanlei2021@outlook.com>
2024-06-11 22:28:32 -0500

    App So Far

--- app/components/Card.js
+++ app/components/Card.js
@@ -5,68 +5,108 @@ import { globalStyles, images } from '../globalStyles/globalStyles';
 export default function Card({ item, handleIcon, handleSteal }){
     return (
         <View style={styles.container}>
-            <Text style={styles.week}>Week {item.key}:</Text>
+
+            {/* Left Week Indicator */}
+            <Text style={styles.label}>Week {item.week}:</Text>
+
+            {/* Background Image */}
             <ImageBackground 
                 source={require('../../assets/images/GardenBackground.jpg')}
                 imageStyle={{resizeMode: 'contain', opacity: 0.7 }}
                 style={styles.container}
             >
-                <TouchableOpacity style={{ marginLeft: -15, 
-                                            marginTop: 5,
-                                            width: 42, 
-                                            height: 45, }}
-                    onPress={() => handleIcon(item.condition1)}>
-                <Image source={ images.condition[item.condition1] } style={[
-                    item.condition1 == 0 && styles.lock,
-                    item.condition1 == 1 && styles.flower,
-                    item.condition1 == 2 && styles.pot,
-                ]}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{ marginTop: 0, 
-                                            width: 42, 
-                                            height: 45, }}
-                    onPress={() => handleIcon(item.condition2)}>
-                <Image source={ images.condition[item.condition2] } style={[
-                    item.condition2 == 0 && styles.lock,
-                    item.condition2 == 1 && styles.flower,
-                    item.condition2 == 2 && styles.pot,
-                ]}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{ marginTop: 5,  
-                                            width: 42, 
-                                            height: 45, }}
-                    onPress={() => handleIcon(item.condition3)}>
-                <Image source={ images.condition[item.condition3] } style={[
-                    item.condition3 == 0 && styles.lock,
-                    item.condition3 == 1 && styles.flower,
-                    item.condition3 == 2 && styles.pot,
-                ]}/>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    style={{    
-                        marginTop: 5, 
-                        width: 42, 
-                        height: 45, 
-                    }}
-                    onPress={() => handleIcon(item.condition4)}>
-                <Image 
-                    source={ images.condition[item.condition4] } 
-                    style={[
-                        item.condition4 == 0 && styles.lock,
-                        item.condition4 == 1 && styles.flower,
-                        item.condition4 == 2 && styles.pot,
+            
+            { item.active ? 
+            
+            (
+                /* Active Week */
+                <View style={{ flexDirection: 'row' }}>
+
+                    {/* Icon 1 */}
+                    <TouchableOpacity 
+                        style={ {
+                            marginLeft: -15,
+                            marginTop: 5,
+                            width: 42, 
+                            height: 45, 
+                        } }
+                        onPress={() => handleIcon(item.conditions[0])}>
+                    <Image source={ images.condition[item.conditions[0]] } style={[
+                        item.conditions[0] == 0 && styles.pot,
+                        item.conditions[0] == 1 && styles.flower,
                     ]}/>
-                </TouchableOpacity>
-                <TouchableOpacity style={{  marginLeft: 15, 
-                                            marginTop: 5, 
-                                            width: 30, 
-                                            height: 30 }}
-                    onPress={() => handleSteal( item.condition1 )}>
-                <Image source={
-                    item.condition1 == 0 ? require('../../assets/images/Invisible.png')
-                    : require('../../assets/images/Bucket.png')
-                } style={styles.steal}/>
-                </TouchableOpacity>
+                    </TouchableOpacity>
+
+                    {/* Icon 2 */}
+                    <TouchableOpacity 
+                        style={{ 
+                            marginTop: 2, 
+                            width: 42, 
+                            height: 45, 
+                        }}
+                        onPress={() => handleIcon(item.conditions[1])}>
+                    <Image source={ images.condition[item.conditions[1]] } style={[
+                        item.conditions[1] == 0 && styles.pot,
+                        item.conditions[1] == 1 && styles.flower,
+                    ]}/>
+                    </TouchableOpacity>
+
+                    {/* Icon 3 */}
+                    <TouchableOpacity 
+                        style={{ 
+                            marginTop: 5,  
+                            width: 42, 
+                            height: 45, 
+                        }}
+                        onPress={() => handleIcon(item.conditions[2])}>
+                    <Image source={ images.condition[item.conditions[2]] } style={[
+                        item.conditions[2] == 0 && styles.pot,
+                        item.conditions[2] == 1 && styles.flower,
+                    ]}/>
+                    </TouchableOpacity>
+
+                    {/* Icon 4 */}
+                    <TouchableOpacity 
+                        style={{    
+                            marginTop: 5, 
+                            width: 42, 
+                            height: 45, 
+                        }}
+                        onPress={() => handleIcon(item.conditions[3])}>
+                    <Image 
+                        source={ images.condition[item.conditions[3]] } 
+                        style={[
+                            item.conditions[3] == 0 && styles.pot,
+                            item.conditions[3] == 1 && styles.flower,
+                        ]}/>
+                    </TouchableOpacity>
+
+                    {/* Steal Icon */}
+                    <TouchableOpacity 
+                        style={ {  
+                            marginLeft: 15, 
+                            marginTop: 5, 
+                            width: 30, 
+                            height: 30 
+                        } }
+                        onPress={() => handleSteal()}
+                    >
+                        <Image 
+                            source={ require('../../assets/images/Bucket.png') } 
+                            style={ styles.steal }
+                        />
+                    </TouchableOpacity>
+
+                </View>
+            ) 
+            : 
+            (
+                /* Inactive Week */
+                <View style={ styles.placeholder }>  
+                </View>
+            )
+            }
+
             </ImageBackground>
         </View>
     )
@@ -80,20 +120,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    week: {
+    label: {
         fontFamily: 'EBG-bold',
-        fontWeight: 'bold',
         fontSize: 15,
         marginRight: 10,
     },
+
+
     flower: {
         marginTop: -15,
         width: 50,
         height: 50,
-    },
-    lock: {
-        width: 40,
-        height: 40,
     },
     pot: {
         marginLeft: 15,
@@ -101,10 +138,15 @@ const styles = StyleSheet.create({
         width: 20,
         height: 20,
     },
+
     steal: {
         marginLeft: 3,
-        marginTop: -5,
+        marginTop: 2,
         width: 30,
         height: 30,
     },
+
+    placeholder: {
+        height: 50
+    }
 })
